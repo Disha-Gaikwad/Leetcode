4568a1b8c7cd728c6d3d87ce4d62d52578a5af5b
Disha Gaikwad <dishagaikwad10a06@gmail.com>
2026-03-06 04:47:44 +0530

    Implement rotateString method for string rotation

---
+++ Leetcode_796.java
@@ -0,0 +1,11 @@
+//Rotate String
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        String temp = s + s;
+        return temp.contains(goal);
+        
+    }
+}
